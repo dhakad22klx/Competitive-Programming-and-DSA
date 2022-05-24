00dae666f231c262340215ff66634408e683d6e6
Deepak DHAKAD <87806512+dhakad22klx@users.noreply.github.com>
2022-05-24 16:24:53 +0530

    Update Q_and_S.cpp

--- Q_and_S.cpp
+++ Q_and_S.cpp
@@ -112,3 +112,23 @@ string Solution::solve(string A) {
 
 }
 
+
+
+
+
+<Question> You are given an integer N and the task is to reverse the digits of the given integer. Return 0 if the result overflows and 
+ does not fit in a 32 bit signed integer.
+ 
+"Solution" : 
+int Solution::reverse(int A) {
+    long int sol = 0;
+    
+    while(A != 0){
+        sol = sol*10 + (A%10);    
+        A = A/10;
+    }
+    if(sol > INT_MAX || sol < INT_MIN){
+        return 0;
+    }
+    return sol;
+}
