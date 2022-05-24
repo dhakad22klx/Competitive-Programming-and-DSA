7c4e60bd733e9307a66acbd91ee998f473466f18
dhakad22klx <dhakad22klx.pvt@gmail.com>
2022-05-24 16:42:10 +0530

    Imp Points

---
+++ notes.cpp
@@ -0,0 +1,26 @@
+//Something We Need To Study 
+ Two pointer algorithm 
+
+
+
+
+
+
+
+
+// Points to be keep in mind
+#Inbuit Algorithms used by sort()(STL)
+The algorithm used by sort() is IntroSort. Introsort being a hybrid sorting algorithm uses three sorting algorithm to minimize the running time, Quicksort, Heapsort and Insertion Sort. Simply putting, it is the best sorting algorithm around. It is a hybrid sorting algorithm, which means that it uses more than one sorting algorithms as a routine
+
+
+
+
+
+
+
+
+
+
+
+
+//General theory 
