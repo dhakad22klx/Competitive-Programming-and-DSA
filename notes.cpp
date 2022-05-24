ddb2b7f354faa8a2838588b42d0bc3c2184fd237
dhakad22klx <dhakad22klx.pvt@gmail.com>
2022-05-24 16:48:23 +0530

    IMP points

--- notes.cpp
+++ notes.cpp
@@ -9,9 +9,11 @@
 
 
 // Points to be keep in mind
-#Inbuit Algorithms used by sort()(STL)
+1.Inbuit Algorithms used by sort()(STL)
 The algorithm used by sort() is IntroSort. Introsort being a hybrid sorting algorithm uses three sorting algorithm to minimize the running time, Quicksort, Heapsort and Insertion Sort. Simply putting, it is the best sorting algorithm around. It is a hybrid sorting algorithm, which means that it uses more than one sorting algorithms as a routine
 
+2.use stoll instead of stoi if we get out of range error.
+also Adding number to INT_MAX gives overflow error and vice versa 
 
 
 
